2eaf255cc64ed2ec4629fe4418dafe758a6406d7
aliabdulameer <aliabdulameerk@gmail.com>
2024-05-17 15:20:23 +0330

    1

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
@@ -11,16 +12,20 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/register', function (Request $request) {
-    return response()->json(['laravel'])->setStatusCode(201);
+ //   $username = $request->get('username');
+   // $password = $request->get('password');
+    return Response::json($request->all())->setStatusCode(201);
+
+
 });
 
 
 Route::get('/posts', function (Request $request) {
 
-    $startDate = Carbon::createFromDate(2015, 5, 17)->getTimestampMs();
-    $endDate = Carbon::createFromDate(2021, 12, 12)->getTimestampMs();
+    $startDate = Carbon::createFromDate($request->get('start_Date'))->getTimestampMs();
+    $endDate  = Carbon::createFromDate($request->get('end_Date'))->getTimestampMs();
 
-    $data = DB::table('posts')->whereBetween('published_at',[$startDate,$endDate])->get();
+    $data = DB::table('posts')->whereBetween('published_at', [$startDate,$endDate])->get();
 
     return response()->json($data)->setStatusCode(200);
 });
