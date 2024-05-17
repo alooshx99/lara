867fa1715a2b8d99a290025e805f72d191c4ff7d
aliabdulameer <aliabdulameerk@gmail.com>
2024-05-17 05:58:55 +0330

    1

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/register', function (Request $request) {
+    return response()->json(['laravel'])->setStatusCode(201);
+});
+
+
+Route::get('/posts', function (Request $request) {
+    $data = DB::table('posts')->get();
+
+    return response()->json($data)->setStatusCode(200);
+});
